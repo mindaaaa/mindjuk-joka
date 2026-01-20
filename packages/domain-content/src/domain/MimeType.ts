96d9cd48fa2ba12bc8a4ccb4bdeea5196d489126
injuk <ingnoh1001@gmail.com>
2026-01-20 20:50:37 +0900

    refactor: MimeType 엔티티에 zod 스키마 개념을 도입

--- packages/domain-content/src/domain/MimeType.ts
+++ packages/domain-content/src/domain/MimeType.ts
@@ -1,29 +1,31 @@
 import * as mime from 'mime-types';
 import { z } from 'zod';
 
-const mimeTypeSchema = z.string().refine(
-  (value) => {
-    /**
-     * MIME 타입 형식 검증
-     * - 알파벳/숫자/하이픈/플러스 조합 (예: png, jpeg, x-www-form-urlencoded)
-     * @example image/png, video/mp4, audio/mpeg
-     */
-    const mimeTypeRegex = /^[a-z]+\/[a-z0-9\-+]+$/i;
-    return mimeTypeRegex.test(value) && !!mime.extension(value);
-  },
-  {
-    message: '유효하지 않은 MIME 타입 형식입니다',
-  },
-);
-
 export class MimeType {
   public readonly type: string;
   public readonly subType: string;
 
   static from(value: unknown): MimeType {
-    mimeTypeSchema.parse(value);
+    MimeType.Schema.parse(value);
 
     return new MimeType(value as string);
+  }
+
+  static get Schema() {
+    return z.string().refine(
+      (value) => {
+        /**
+         * MIME 타입 형식 검증
+         * - 알파벳/숫자/하이픈/플러스 조합 (예: png, jpeg, x-www-form-urlencoded)
+         * @example image/png, video/mp4, audio/mpeg
+         */
+        const mimeTypeRegex = /^[a-z]+\/[a-z0-9\-+]+$/i;
+        return mimeTypeRegex.test(value) && !!mime.extension(value);
+      },
+      {
+        message: '유효하지 않은 MIME 타입 형식입니다',
+      },
+    );
   }
 
   private constructor(value: string) {
