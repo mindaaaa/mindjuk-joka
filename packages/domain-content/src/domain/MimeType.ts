db49fccf1f1e29d5e78c1c0fec08a2efb0132eed
injuk <ingnoh1001@gmail.com>
2026-01-20 20:32:13 +0900

    feat: MimeType 엔티티 정의

---
+++ packages/domain-content/src/domain/MimeType.ts
@@ -0,0 +1,39 @@
+import * as mime from 'mime-types';
+import { z } from 'zod';
+
+const mimeTypeSchema = z.string().refine(
+  (value) => {
+    /**
+     * MIME 타입 형식 검증
+     * - 알파벳/숫자/하이픈/플러스 조합 (예: png, jpeg, x-www-form-urlencoded)
+     * @example image/png, video/mp4, audio/mpeg
+     */
+    const mimeTypeRegex = /^[a-z]+\/[a-z0-9\-+]+$/i;
+    return mimeTypeRegex.test(value) && !!mime.extension(value);
+  },
+  {
+    message: '유효하지 않은 MIME 타입 형식입니다',
+  },
+);
+
+export class MimeType {
+  public readonly type: string;
+  public readonly subType: string;
+
+  static from(value: unknown): MimeType {
+    mimeTypeSchema.parse(value);
+
+    return new MimeType(value as string);
+  }
+
+  private constructor(value: string) {
+    const [type, subType] = value.split('/');
+
+    this.type = type as string;
+    this.subType = subType as string;
+  }
+
+  get value() {
+    return [this.type, this.subType].join('/');
+  }
+}
