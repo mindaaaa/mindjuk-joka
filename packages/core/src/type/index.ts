c0d4fd5738a7dc657fb7212ab8ca1d62320ce04b
injuk <ingnoh1001@gmail.com>
2026-01-26 20:37:04 +0900

    refactor: 불필요한 interface 제거

--- packages/core/src/type/index.ts
+++ packages/core/src/type/index.ts
@@ -1,16 +1,3 @@
-import { Email } from "../model/Email";
-
-export interface Actioned {
-    at: Date;
-    by: ActionedBy;
-}
-
-export interface ActionedBy {
-    id: string;
-    name: string;
-    email: Email;
-}
-
 export type Nullable<T> = T | null;
 
 export type Nullish<T> = T | null | undefined;
