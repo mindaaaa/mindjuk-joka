5ffd858b61ea92aaff3fb5142ddcf138544cd44a
injuk <ingnoh1001@gmail.com>
2026-02-02 18:54:39 +0900

    feat: Media 서비스 작성 완료

--- packages/domain-media/src/service/media.service.ts
+++ packages/domain-media/src/service/media.service.ts
@@ -1,36 +1,98 @@
-import { NotImplementedException } from '@joka/core/src/exception';
+import { Album } from '@joka/core/src/model/Album';
+import { User } from '@joka/core/src/model/User';
+import { Nullable } from '@joka/core/src/type';
 
-export const create = async (): Promise<never> => {
-  throw new NotImplementedException(`MEDIA_SERVICE_NOT_IMPLEMENTED`, [
-    `구현되지 않은 API입니다.`,
-    `관리자에게 문의하세요.`,
-  ]);
+import { ListMediaCondition } from '../domain/ListMediaCondition';
+import { Media, DraftMedia } from '../domain/Media';
+import { UpdateMediaRequest } from '../domain/UpdateMediaRequest';
+import * as repository from '../infrastructure/persistence/media.repository';
+
+interface Context {
+  album: Album;
+  user: User;
+}
+
+interface CreateRequest {
+  description: string;
+}
+export const create = (
+  context: Context,
+  request: CreateRequest,
+): Promise<Media> => {
+  return repository.insert(
+    DraftMedia.from({
+      album: context.album,
+      user: context.user,
+      description: request.description,
+    }),
+  );
 };
 
-export const list = async (): Promise<never> => {
-  throw new NotImplementedException(`MEDIA_SERVICE_NOT_IMPLEMENTED`, [
-    `구현되지 않은 API입니다.`,
-    `관리자에게 문의하세요.`,
-  ]);
+interface ListRequest {
+  limit?: number;
+  states?: string[];
+  cursor?: string;
+  sortOrder?: string;
+}
+interface ListResponse<E, P> {
+  items: E[];
+  pagination: { size: number } & P;
+}
+export const list = async (
+  context: Context,
+  request: ListRequest,
+): Promise<
+  ListResponse<
+    Media,
+    { order: string; nextCursor: Nullable<string>; hasNext: boolean }
+  >
+> => {
+  const condition = ListMediaCondition.from({
+    limit: request.limit,
+    filter: { albumId: context.album.id, states: request.states || [] },
+    cursor: request.cursor ? { cid: request.cursor } : null,
+    sortOrder: request.sortOrder,
+  });
+
+  const { items, nextCursor } = await repository.findMany(condition);
+
+  return {
+    items,
+    pagination: {
+      size: condition.limit,
+      order: condition.sortOrder,
+      nextCursor: nextCursor ? nextCursor.cid : null,
+      hasNext: !!nextCursor,
+    },
+  };
 };
 
-export const get = async (): Promise<never> => {
-  throw new NotImplementedException(`MEDIA_SERVICE_NOT_IMPLEMENTED`, [
-    `구현되지 않은 API입니다.`,
-    `관리자에게 문의하세요.`,
-  ]);
+interface GetRequest {
+  cid: string;
+}
+export const get = (context: Context, request: GetRequest): Promise<Media> => {
+  return repository.findOne(context.album.id, request.cid);
 };
 
-export const update = async (): Promise<never> => {
-  throw new NotImplementedException(`MEDIA_SERVICE_NOT_IMPLEMENTED`, [
-    `구현되지 않은 API입니다.`,
-    `관리자에게 문의하세요.`,
-  ]);
+export const update = async (
+  context: Context,
+  request: UpdateMediaRequest,
+): Promise<Media> => {
+  const found = await repository.findOne(context.album.id, request.cid);
+
+  const desired = found.updateBy(request);
+
+  return repository.update(desired);
 };
 
-export const remove = async (): Promise<never> => {
-  throw new NotImplementedException(`MEDIA_SERVICE_NOT_IMPLEMENTED`, [
-    `구현되지 않은 API입니다.`,
-    `관리자에게 문의하세요.`,
-  ]);
+interface DeleteRequest {
+  cid: string;
+}
+export const remove = async (
+  context: Context,
+  request: DeleteRequest,
+): Promise<null> => {
+  const target = await repository.findOne(context.album.id, request.cid);
+
+  return repository.deleteOne(target);
 };
