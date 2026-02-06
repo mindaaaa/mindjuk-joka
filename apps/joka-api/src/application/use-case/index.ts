a28f8a99d8bea4e0bf840a0300520d05e6a1dd2b
injuk <ingnoh1001@gmail.com>
2026-02-06 20:46:15 +0900

    feat: 컨트롤러 호출시 유즈케이스가 호출되도록 개선

---
+++ apps/joka-api/src/application/use-case/index.ts
@@ -0,0 +1,5 @@
+// commands
+export { default as CreateMedia } from './command/CreateMediaImpl';
+
+// queries
+export { default as GetMedia } from './query/GetMediaImpl';
--- apps/joka-api/src/infrastructure/web/v1/media.controller.ts
+++ apps/joka-api/src/infrastructure/web/v1/media.controller.ts
@@ -1,15 +1,46 @@
+import { Album } from '@joka/core/src/model/Album';
+import { User } from '@joka/core/src/model/User';
 import { Hono } from 'hono';
+
+import { CreateMedia, GetMedia } from '../../../application/use-case';
 
 const media = new Hono().basePath('/v1/media');
 
-media.post('/', (c) => {
-  console.log(JSON.stringify(c, null, 2));
-  return c.text('Crate Media');
+const getMockContext = () => ({
+  album: Album.from({
+    id: 1,
+    cid: '7b799ad4-41b5-4f66-ba7c-b6f148f64f00',
+    name: 'dev',
+    description: 'for test',
+    isDeleted: false,
+  }),
+  user: User.from({
+    id: 1,
+    cid: 'efbef729-c015-437e-a8ff-72e77f589038',
+    name: 'tester',
+    email: 'tester@naver.com',
+  }),
 });
 
-media.get('/:cid', (c) => {
-  console.log(JSON.stringify(c, null, 2));
-  return c.text('Get Media');
+media.post('/', async (c) => {
+  const body = await c.req.json();
+  const description = body.description;
+
+  const result = await CreateMedia.invoke({ ...getMockContext(), description });
+
+  return c.json(result.data, 201, {
+    'Content-Type': 'application/json',
+  });
+});
+
+media.get('/:cid', async (c) => {
+  const mediaCid = c.req.param('cid');
+
+  const result = await GetMedia.invoke({ ...getMockContext(), mediaCid });
+
+  return c.json(result.data, 200, {
+    'Content-Type': 'application/json',
+  });
 });
 
 export default media;
