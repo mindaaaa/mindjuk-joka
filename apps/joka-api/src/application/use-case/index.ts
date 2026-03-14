b5ed5d793f1a230ab4ace320fac7e2a82e3d1ed5
injuk <ingnoh1001@gmail.com>
2026-03-14 22:30:42 +0900

    feat: 목록 조회 API를 컨트롤러에 정합

--- apps/joka-api/src/application/use-case/index.ts
+++ apps/joka-api/src/application/use-case/index.ts
@@ -3,3 +3,4 @@ export { default as CreateMedia } from './command/CreateMediaImpl';
 
 // queries
 export { default as GetMedia } from './query/GetMediaImpl';
+export { default as ListMedia } from './query/ListMediaImpl';
--- apps/joka-api/src/infrastructure/web/v1/media.controller.ts
+++ apps/joka-api/src/infrastructure/web/v1/media.controller.ts
@@ -2,7 +2,11 @@ import { Album } from '@joka/core/src/model/Album';
 import { User } from '@joka/core/src/model/User';
 import { Hono } from 'hono';
 
-import { CreateMedia, GetMedia } from '../../../application/use-case';
+import {
+  CreateMedia,
+  GetMedia,
+  ListMedia,
+} from '../../../application/use-case';
 
 const media = new Hono().basePath('/v1/media');
 
@@ -31,6 +35,41 @@ media.post('/', async (c) => {
   return c.json(result.data, 201, {
     'Content-Type': 'application/json',
   });
+});
+
+media.get('/', async (c) => {
+  const invokeRequest: Record<string, string> = {};
+  const size = c.req.query('size');
+  const order = c.req.query('order');
+  const cursor = c.req.query('cursor');
+  const states = c.req.query('states');
+
+  if (size) {
+    invokeRequest.size = size;
+  }
+  if (order) {
+    invokeRequest.order = order;
+  }
+  if (cursor) {
+    invokeRequest.cursor = cursor;
+  }
+  if (states) {
+    invokeRequest.states = states;
+  }
+
+  const result = await ListMedia.invoke({
+    ...getMockContext(),
+    ...invokeRequest,
+  });
+
+  return c.json(
+    {
+      items: result.items.map((item) => item.data),
+      pagination: result.pagination,
+    },
+    200,
+    { 'Content-Type': 'application/json' },
+  );
 });
 
 media.get('/:cid', async (c) => {
