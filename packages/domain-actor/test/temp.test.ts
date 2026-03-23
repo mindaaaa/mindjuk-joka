aa1dc343bc83938c7ce83b71c0ce79075d28bbc6
injuk <ingnoh1001@gmail.com>
2026-03-23 19:34:10 +0900

    chore: domain-actor 모듈 정의

---
+++ packages/domain-actor/test/temp.test.ts
@@ -0,0 +1,13 @@
+describe('temp', () => {
+  it('1 + 1 = 2', async () => {
+    // given
+    const a = 1;
+    const b = 1;
+
+    // when
+    const result = a + b;
+
+    // then
+    expect(result).toBe(2);
+  });
+});
