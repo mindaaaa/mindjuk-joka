756f32765b0efec54ef17c49ae3dfe33c8ff1ecb
injuk <ingnoh1001@gmail.com>
2026-04-30 20:57:30 +0900

    feat: DraftUser 도메인 클래스 추가

--- packages/core/src/model/User.ts
+++ packages/core/src/model/User.ts
@@ -9,6 +9,44 @@ interface ConstructorParameters {
   email: string;
 }
 
+export class DraftUser {
+  static from(
+    params: Omit<ConstructorParameters, 'id' | 'cid'> & { provider: string },
+  ): DraftUser {
+    const email = Email.from(params.email);
+    const draft = new DraftUser(params.name, email, params.provider);
+
+    DraftUser.Schema.parse(draft);
+
+    return draft;
+  }
+
+  static get Schema() {
+    return z.object({
+      name: z.string().min(1),
+      email: Email.Schema,
+      provider: z.string().min(1),
+    });
+  }
+
+  private constructor(
+    public readonly name: string,
+    public readonly email: Email,
+    public readonly provider: string,
+  ) {}
+
+  get data() {
+    const draft = {
+      ...this,
+      email: this.email.value,
+    };
+
+    DraftUser.Schema.parse(draft);
+
+    return draft;
+  }
+}
+
 export class User {
   static from(params: ConstructorParameters): User {
     User.Schema.parse(params);
