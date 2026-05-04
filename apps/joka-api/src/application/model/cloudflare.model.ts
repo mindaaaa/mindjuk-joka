9220663b924915721f77b36539db226b2986f4f8
injuk <ingnoh1001@gmail.com>
2026-05-05 00:53:29 +0900

    feat: 컨트롤러와 미들웨어를 애플리케이션에 연결

--- apps/joka-api/src/application/model/cloudflare.model.ts
+++ apps/joka-api/src/application/model/cloudflare.model.ts
@@ -1,4 +1,5 @@
 import { Actor } from '@joka/domain-actor/src/domain/Actor';
+import type { JwtPayload } from 'packages/domain-auth/src/domain/helper/jwt.provider';
 
 export interface CloudflareEnv {
   Bindings: {
@@ -9,8 +10,14 @@ export interface CloudflareEnv {
     OBJECT_STORAGE_SECRET_ACCESS_KEY: string;
     OBJECT_STORAGE_ENDPOINT: string;
     OBJECT_STORAGE_BUCKET_NAME: string;
+    AUTH_TOKENS: KVNamespace;
+    KAKAO_CLIENT_ID: string;
+    KAKAO_CLIENT_SECRET: string;
+    KAKAO_REDIRECT_URI: string;
+    JWT_SECRET: string;
   };
   Variables: {
     actor: Actor;
+    jwtPayload: JwtPayload;
   };
 }
