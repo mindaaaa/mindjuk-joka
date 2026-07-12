f9cd1c578c1d461f0b5af6baa16499f72f330a3f
injuk <ingnoh1001@gmail.com>
2026-07-12 23:22:53 +0900

    feat: user-events 저장 API 추가

--- apps/joka-api/src/application/use-case/index.ts
+++ apps/joka-api/src/application/use-case/index.ts
@@ -5,6 +5,7 @@ export { default as DeleteMedia } from './command/DeleteMediaImpl';
 export { default as CreateUploadUrlForMedia } from './command/CreateUploadUrlForMediaImpl';
 export { default as CreateContent } from './command/CreateContentImpl';
 export { default as ConfirmMedia } from './command/ConfirmMediaImpl';
+export { default as CreateUserEvents } from './command/CreateUserEventsImpl';
 
 // queries
 export { default as GetMedia } from './query/GetMediaImpl';
